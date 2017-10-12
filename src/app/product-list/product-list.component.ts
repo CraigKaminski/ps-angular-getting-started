ae021c366cdce94620743afa7dc2b583ce0eaaa4
Craig Kaminski <ckaminski@everesttech.com>
2017-10-12 12:14:09 -0400

    Make filter in product list component functional

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -11,7 +11,9 @@ export class ProductListComponent implements OnInit {
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
-  listFilter: string = 'cart';
+  _listFilter: string;
+  filteredProducts: IProduct[];
+
   products: IProduct[] = [
     {
       "productId": 1,
@@ -45,12 +47,31 @@ export class ProductListComponent implements OnInit {
     }
   ];
 
-  constructor() { }
+  constructor() {
+    this.filteredProducts = this.products;
+    this.listFilter = 'cart';
+  }
 
   ngOnInit() {
   }
 
   toggleImage(): void {
     this.showImage = !this.showImage;
+  }
+
+  get listFilter(): string {
+    return this._listFilter;
+  }
+
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+  }
+
+  performFilter(filterBy: string): IProduct[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter((product: IProduct) =>
+      product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1
+    );
   }
 }
