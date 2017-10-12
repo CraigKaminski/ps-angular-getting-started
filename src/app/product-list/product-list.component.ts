255ede5b159e5e957480a35d6f2cb56765f269fa
Craig Kaminski <ckaminski@everesttech.com>
2017-10-12 13:27:40 -0400

    Replace number in start rating filed of product list component with star component and display numeric value of star rating in title if start component is clicked

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -8,6 +8,7 @@ import { IProduct } from './product';
 })
 export class ProductListComponent implements OnInit {
   pageTitle: string = 'Product List';
+  rating: number;
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
@@ -73,5 +74,9 @@ export class ProductListComponent implements OnInit {
     return this.products.filter((product: IProduct) =>
       product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1
     );
+  }
+
+  onRatingClicked(message: string): void {
+    this.pageTitle = 'Product List: ' + message;
   }
 }
