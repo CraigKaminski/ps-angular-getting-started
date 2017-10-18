8c2e96886d11d6113126225fb8a06124615b872b
Craig Kaminski <ckaminski@everesttech.com>
2017-10-18 14:57:58 -0400

    Add retrieving a single product to product service and update product details component to use it

--- src/app/product/product.service.ts
+++ src/app/product/product.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/throw';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/map';
 
 import { IProduct } from './product';
 
@@ -16,6 +17,15 @@ export class ProductService {
   getProducts(): Observable<IProduct[]> {
     return this._http.get<IProduct[]>(this._productUrl)
       .do(data => console.log('All: ' + JSON.stringify(data)))
+      .catch(this.handleError);
+  }
+
+  getProduct(id: number): Observable<IProduct> {
+    return this._http.get<IProduct[]>(this._productUrl)
+      .map(products => {
+        const filteredProducts = products.filter(product => product.productId === id);
+        return (filteredProducts.length > 0) ? filteredProducts[0] : null;
+      })
       .catch(this.handleError);
   }
 
